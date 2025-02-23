896b8d3b538ecd636c9fdcc4803031eba968dad0
tf7software <160179762+tf7software@users.noreply.github.com>
2025-02-23 09:21:14 -0700

    Try to fix typerror

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://forg.gaudencio.net.br/bare/',
+    bare:'https://bare-proxy.lupinevault.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
